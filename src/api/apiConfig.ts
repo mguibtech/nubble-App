2923d365bbe957524b173a9d078b15db7855e12d
mguibson <marcos.silva@softlive.dev>
2025-09-11 01:14:57 -0300

    fix - Corrigir URL base da API para localhost e adicionar tratamento de erro na função getList do postAPI

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://127.0.0.1:3333/',
+  baseURL: 'http://localhost:3333/',
   headers: {
     Authorization:
       'Bearer OQ.BoxIVc3DcFyfT4Hp79yZxrPxvm8a8GT4AgTbMCLCEKe916O71yU6E2vFa8IK',
--- src/domain/Post/postAPI.ts
+++ src/domain/Post/postAPI.ts
@@ -3,9 +3,14 @@ import {api, PageAPI} from '@api';
 import {PostAPI} from './postType';
 
 async function getList(): Promise<PageAPI<PostAPI>> {
-  const response = await api.get<PageAPI<PostAPI>>('user/post');
-  console.log('getList', response.data);
-  return response.data;
+  try {
+    const response = await api.get<PageAPI<PostAPI>>('user/post');
+    console.log('getList', response.data);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching post list:', error);
+    throw error;
+  }
 }
 
 export const postAPI = {
